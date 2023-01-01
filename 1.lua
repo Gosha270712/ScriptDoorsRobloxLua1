20481c69491f8c6ff108312361fc54c795da8a97
Gosha <116460911+Gosha270712@users.noreply.github.com>
2023-01-02 01:01:40 +0300

    Add files via upload

---
+++ 1.lua
@@ -0,0 +1,15 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("DoorsScriptC, "DarkTheme")
+
+--MAIN
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Main")
+
+
+MainSection:NewButton("Circufir", "Magic", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/EKP8bDMi'))()
+end)
+
+MainSection:NewButton("SummonS", "SummonScript", function()
+    loadstring(game:HttpGet"'https://raw.githubusercontent.com/plamen6789/DoorsEntitySummonerGUI/main/EntityGUI'))()")()
+end)
